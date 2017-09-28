33b0cefb301d89db0e7dc9311777f4c6f6cc936e
Denys P <dector9@gmail.com>
2017-09-28 20:39:09 +0300

    Display favourite games

--- app/src/main/java/io/github/dector/rldb/favourites/view/controllers/FavouritesController.kt
+++ app/src/main/java/io/github/dector/rldb/favourites/view/controllers/FavouritesController.kt
@@ -1,13 +1,31 @@
 package io.github.dector.rldb.favourites.view.controllers
 
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.Controller
 import io.github.dector.rldb.R
+import io.github.dector.rldb.common.repositories.InMemoryGamesRepository
+import io.github.dector.rldb.games_list.view.adapters.ListItemsAdapter
+import io.github.dector.rldb.games_list.viewmodels.ListItemViewModel
+import kotlinx.android.synthetic.main.controller_favourites.view.*
 
 
 class FavouritesController : Controller() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup)
-            = inflater.inflate(R.layout.controller_favourites, container, false)
+            = inflater.inflate(R.layout.controller_favourites, container, false).apply {
+        list.setHasFixedSize(true)
+        list.layoutManager = LinearLayoutManager(inflater.context)
+
+        list.adapter = ListItemsAdapter(inflater, {}).apply {
+            data = InMemoryGamesRepository().getFavourite().map {
+                ListItemViewModel(
+                        uuid = it.uuid,
+                        title = it.name,
+                        description = it.description,
+                        imageUrl = it.imageUrl ?: "")
+            }
+        }
+    }
 }
